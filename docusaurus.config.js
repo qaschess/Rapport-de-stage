12fb894d8034d468534b4625d4568ee4b40fa37b
Qassim <qassimtaleb20@gmail.com>
2023-03-24 17:18:43 +0100

    commit change

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Mon Site',
+  tagline: 'Mon rapport de stage',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
@@ -74,24 +74,7 @@ const config = {
       },
       footer: {
         style: 'dark',
-        links: [
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
+        links: [          
           {
             title: 'More',
             items: [
@@ -101,7 +84,7 @@ const config = {
               },
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/qaschess/Rapport-de-stage',
               },
             ],
           },
