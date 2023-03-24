825271a0779a6566d1014fc456527810854710a7
Qassim <qassimtaleb20@gmail.com>
2023-03-24 17:22:41 +0100

    commit change

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,7 +43,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/antoine-cultru/Blog/tree/main/blog'
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
